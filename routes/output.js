bae7af18985886a69d70a6ca8046e7b635018bdd
Jakub Brockl <brockljakub@gmail.com>
2019-06-11 19:58:42 +0200

    Add output section

--- routes/output.js
+++ routes/output.js
@@ -1,9 +1,9 @@
 var express = require("express");
 var router = express.Router();
 
-/* GET users listing. */
+/* GET home page. */
 router.get("/", function(req, res, next) {
-  res.send("respond with a resource");
+  res.render("output", { title: "Efata 1.0" });
 });
 
 module.exports = router;
