490b352a4ac5a9422478aa6ccfd4849b997501dd
Jakub Brockl <brockljakub@gmail.com>
2019-06-18 23:31:55 +0200

    Add database url env

--- app.js
+++ app.js
@@ -21,11 +21,7 @@ app.set("view engine", "pug");
 typeorm
   .createConnection({
     type: process.env.TYPEORM_CONNECTION,
-    host: process.env.TYPEORM_HOST,
-    port: process.env.TYPEORM_PORT,
-    username: process.env.TYPEORM_USERNAME,
-    password: process.env.TYPEORM_PASSWORD,
-    database: process.env.TYPEORM_DATABASE,
+    url: process.env.DATABASE_URL,
     synchronize: true,
     logging: false,
     entities: [require("./entity/RecordSchema")]
