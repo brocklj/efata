25006746ac771b44d13fe95323a64cb8270f0bdf
Jakub Brockl <brockljakub@gmail.com>
2019-07-07 22:15:36 +0200

    Add export

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -11,3 +11,21 @@ function drop(ev) {
   var data = ev.dataTransfer.getData("text");
   ev.target.appendChild(document.getElementById(data));
 }
+
+function printDiv(divName) {
+  var mywindow = window.open("", "PRINT", "height=800,width=800");
+
+  mywindow.document.write("<html><head><title>" + document.title + "</title>");
+  mywindow.document.write("</head><body >");
+  mywindow.document.write("<h1>" + document.title + "</h1>");
+  mywindow.document.write(document.getElementById(divName).innerHTML);
+  mywindow.document.write("</body></html>");
+
+  mywindow.document.close(); // necessary for IE >= 10
+  mywindow.focus(); // necessary for IE >= 10*/
+
+  mywindow.print();
+  mywindow.close();
+
+  return true;
+}
