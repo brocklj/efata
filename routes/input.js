d6d0d2e48bcf46c99e05977d852eaf23c4fb495e
Jakub Brockl <brockljakub@gmail.com>
2019-07-17 10:45:33 +0200

    Add ingnore invalid data

--- routes/input.js
+++ routes/input.js
@@ -15,20 +15,25 @@ router.post("/", upload.single("file"), async (req, res) => {
   try {
     const buffer = req.file.buffer.toString();
 
-    const output = parse(
-      `"code","time","date","reader"\n` + buffer.replace(`\r`, ``),
-      {
-        columns: true,
-        skip_empty_lines: true,
-        trim: true
-      }
-    );
+    const output = parse(`"code","time","date","reader"\r\n` + buffer, {
+      columns: true,
+      skip_empty_lines: true,
+      trim: true
+    });
 
     const rg = new RecordGenerator();
     var records = rg.processData(output);
 
     const postRepository = getRepository(Record);
-    await postRepository.save(records);
+
+    for (let i in records) {
+      let record = records[i];
+      try {
+        await postRepository.save(record);
+      } catch (error) {
+        console.log(error);
+      }
+    }
     return res.status(200).json(records);
   } catch (error) {
     console.log(error);
@@ -39,7 +44,8 @@ router.post("/", upload.single("file"), async (req, res) => {
           "V souboru jsou duplicitní záznamy, které se shodují s databazí.";
         break;
       default:
-        message = "Chybný soubor";
+        message = "Chybný soubor\n\n";
+        message += error.message;
     }
     return res.status(400).send(message);
   }
